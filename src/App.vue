0b91c1b5a19859ea4dda95e0f9d77e9cff6cdadd
Anton Popov <anopopov@ozon.ru>
2026-03-14 01:09:20 +0300

    DZ_3: done

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+const timeNow = new Date();
+</script>
+
+<template>
+  <header></header>
+  <main>{{ timeNow.toString() }}</main>
+  <footer></footer>
+</template>
+
+<style scoped>
+</style>
